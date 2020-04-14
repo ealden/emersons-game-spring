81924d4cb6d0bfad1717b3ffef47f001e97fdc21
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-14 11:10:44 +0800

    Spring Boot Application

---
+++ src/test/java/com/escanan/ealden/race/RaceApplicationTest.java
@@ -0,0 +1,11 @@
+package com.escanan.ealden.race;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceApplicationTest {
+    @Test
+    void contextLoads() {
+    }
+}
