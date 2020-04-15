0f0d036fcfb9c31dbf68b0216ec44b604cff4429
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-15 20:00:20 +0800

    RacerRepository interface

---
+++ src/main/java/com/escanan/ealden/race/data/RacerRepository.java
@@ -0,0 +1,7 @@
+package com.escanan.ealden.race.data;
+
+import com.escanan.ealden.race.model.Racer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RacerRepository extends CrudRepository<Racer, Long> {
+}
