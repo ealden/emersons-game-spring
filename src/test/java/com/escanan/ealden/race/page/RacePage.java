97c23baa13b16a55d6a9ec2bf8caac55f81f9614
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-16 05:19:03 +0800

    Expose roll() so we can simply our steps (and add wait.until()...)

--- src/test/java/com/escanan/ealden/race/page/RacePage.java
+++ src/test/java/com/escanan/ealden/race/page/RacePage.java
@@ -1,20 +1,57 @@
 package com.escanan.ealden.race.page;
 
+import com.escanan.ealden.race.model.Racer;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 
 public class RacePage {
     private static final String ROOT_URL = "http://localhost:8080/";
 
     private WebDriver driver = WebDrivers.instance();
+    private WebDriverWait wait = new WebDriverWait(driver, 20);
 
     private RacePage() {
         driver.navigate().to(ROOT_URL);
+
+        wait.until(visibilityOf(getHeader()));
     }
 
     public static RacePage open() {
         return new RacePage();
+    }
+
+    public RacePage roll(int roll, Racer.SpeedType speedType) {
+        if (Racer.SpeedType.NORMAL == speedType) {
+            rollNormalSpeed(roll);
+        } else if (Racer.SpeedType.SUPER == speedType) {
+            rollSuperSpeed(roll);
+        }
+
+        return this;
+    }
+
+    public RacePage rollNormalSpeed(int roll) {
+        getRollInput().sendKeys("" + roll);
+
+        getNormalSpeedButton().click();
+
+        waitUntilNextTurn();
+
+        return this;
+    }
+
+    public RacePage rollSuperSpeed(int roll) {
+        getRollInput().sendKeys("" + roll);
+
+        getSuperSpeedButton().click();
+
+        waitUntilNextTurn();
+
+        return this;
     }
 
     public WebElement getHeader() {
@@ -31,5 +68,9 @@ public class RacePage {
 
     public WebElement getSuperSpeedButton() {
         return driver.findElement(By.id("roll-super-speed"));
+    }
+
+    private void waitUntilNextTurn() {
+        wait.until(textToBePresentInElementValue(getRollInput(), ""));
     }
 }
