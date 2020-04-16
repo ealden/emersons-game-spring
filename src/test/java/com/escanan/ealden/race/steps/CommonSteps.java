7f9e06461334477a1dc4ee2c91fa292d140361b1
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-17 01:42:07 +0800

    Do not close Chrome for now as it slows things down

--- src/test/java/com/escanan/ealden/race/steps/CommonSteps.java
+++ src/test/java/com/escanan/ealden/race/steps/CommonSteps.java
@@ -1,7 +1,6 @@
 package com.escanan.ealden.race.steps;
 
 import com.escanan.ealden.race.data.RacerRepository;
-import com.escanan.ealden.race.page.WebDrivers;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +16,5 @@ public class CommonSteps {
 
     @After
     public void closeBrowser() {
-        WebDrivers.close();
     }
 }
