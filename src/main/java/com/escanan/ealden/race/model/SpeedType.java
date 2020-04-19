0585bf7731c5d04a70dacce811bfc53658626d16
Ealden Esto E. Escañan <ealden@escanan.com>
2020-04-19 19:06:23 +0800

    TEST calculate

--- src/main/java/com/escanan/ealden/race/model/SpeedType.java
+++ src/main/java/com/escanan/ealden/race/model/SpeedType.java
@@ -1,5 +1,22 @@
 package com.escanan.ealden.race.model;
 
 public enum SpeedType {
-    NORMAL, SUPER
+    NORMAL  (1, 2, 1, 0),
+    SUPER   (0, 7, 0, 2);
+
+    private int prefix;
+    private int modulo;
+    private int offset;
+    private int damage;
+
+    SpeedType(int prefix, int modulo, int offset, int damage) {
+        this.prefix = prefix;
+        this.modulo = modulo;
+        this.offset = offset;
+        this.damage = damage;
+    }
+
+    public int calculate(int roll) {
+        return (((prefix + roll) % modulo) + offset);
+    }
 }
---
+++ src/test/java/com/escanan/ealden/race/model/SpeedTypeTest.java
@@ -0,0 +1,28 @@
+package com.escanan.ealden.race.model;
+
+import org.junit.Test;
+
+import static com.escanan.ealden.race.model.SpeedType.NORMAL;
+import static com.escanan.ealden.race.model.SpeedType.SUPER;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class SpeedTypeTest {
+    @Test
+    public void calculate() {
+        assertThat(NORMAL.calculate(1), is(equalTo(1)));
+        assertThat(NORMAL.calculate(2), is(equalTo(2)));
+        assertThat(NORMAL.calculate(3), is(equalTo(1)));
+        assertThat(NORMAL.calculate(4), is(equalTo(2)));
+        assertThat(NORMAL.calculate(5), is(equalTo(1)));
+        assertThat(NORMAL.calculate(6), is(equalTo(2)));
+
+        assertThat(SUPER.calculate(1), is(equalTo(1)));
+        assertThat(SUPER.calculate(2), is(equalTo(2)));
+        assertThat(SUPER.calculate(3), is(equalTo(3)));
+        assertThat(SUPER.calculate(4), is(equalTo(4)));
+        assertThat(SUPER.calculate(5), is(equalTo(5)));
+        assertThat(SUPER.calculate(6), is(equalTo(6)));
+    }
+}
