bc779002bc4e39d9569028e43bf3b30bc000ae15
Ealden Esto E. Escañan <ealden@escanan.com>
2020-07-25 18:29:33 +0800

    More convenience methods

--- src/main/java/com/escanan/ealden/race/model/Race.java
+++ src/main/java/com/escanan/ealden/race/model/Race.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import static com.escanan.ealden.race.model.SpeedType.NORMAL;
 import static java.lang.String.format;
 import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.FetchType.EAGER;
@@ -84,13 +85,13 @@ public class Race {
     }
 
     public String getMessage() {
-        if ((currentRacer != null) && (getLastRoll() == null)) {
+        if (isTimeToRace()) {
             return format("Time to RACE!  %s rolls first!", currentRacer.getName());
         } else if (isAllCrashed()) {
             return "All racers CRASHED!!!  This race is over!";
         } else if (isOver()) {
             return format("%s wins the race!  Congratulations!!!", lastRoll.getRacer().getName());
-        } else if ((lastRoll != null) && lastRoll.getRacer().isCrashed()) {
+        } else if (isRacing() && lastRoll.getRacer().isCrashed()) {
             return format("%s chose %s speed, and rolled %d and moved %d.  %s CRASHED!!!  %s rolls next!",
                     lastRoll.getRacer().getName(),
                     lastRoll.getSpeedType().toString().toUpperCase(),
@@ -98,7 +99,7 @@ public class Race {
                     lastRoll.getMove(),
                     lastRoll.getRacer().getName(),
                     currentRacer.getName());
-        } else if ((lastRoll != null) && (lastRoll.getSpeedType() == SpeedType.NORMAL) && (lastRoll.getRacer().getDamage() > 0)) {
+        } else if (isRacing() && lastRoll.getRacer().isDamaged() && (NORMAL == lastRoll.getSpeedType())) {
             return format("%s chose %s speed, and rolled %d and moved %d.  %s has %d damage.  %s rolls next!",
                     lastRoll.getRacer().getName(),
                     lastRoll.getSpeedType().toString().toUpperCase(),
@@ -110,6 +111,14 @@ public class Race {
         } else {
             return null;
         }
+    }
+
+    private boolean isTimeToRace() {
+        return ((currentRacer != null) && (lastRoll == null));
+    }
+
+    private boolean isRacing() {
+        return ((currentRacer != null) && (lastRoll != null));
     }
 
     public Long getId() {
